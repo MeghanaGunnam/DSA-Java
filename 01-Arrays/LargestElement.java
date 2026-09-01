eca40c5deee322881a724a457eb514ee89aecbd0
MeghanaGunnam <meghana.evm@gmail.com>
2026-09-01 09:47:21 +0530

    Update LargestElement.java

--- 01-Arrays/LargestElement.java
+++ 01-Arrays/LargestElement.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static int largest(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 15, 6, 2, 11};  
+        System.out.println(largest(arr));   
+}
+}
 
