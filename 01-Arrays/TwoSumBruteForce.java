6c4db574ec6fbd0337ba275d02bc04f55040bdd0
MeghanaGunnam <meghana.evm@gmail.com>
2026-09-01 09:51:17 +0530

    Update TwoSumBruteForce.java

--- 01-Arrays/TwoSumBruteForce.java
+++ 01-Arrays/TwoSumBruteForce.java
@@ -1,1 +1,29 @@
+public class TwoSumBruteForce {
+    public static int[] twoSum(int[] nums, int target) {
+        // Check every pair of numbers
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    // Return the indices if sum matches target
+                    return new int[]{i, j};
+                }
+            }
+        }
+        // Return empty array if no solution found
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
 
