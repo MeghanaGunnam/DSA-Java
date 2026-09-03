9709a73533b58cd28a40bce35e61bb73e0dff433
MeghanaGunnam <meghana.evm@gmail.com>
2026-09-03 21:53:50 +0530

    Update LC-217_Contains Duplicate.java

--- 01-Arrays/Leetcode/LC-217_Contains Duplicate.java
+++ 01-Arrays/Leetcode/LC-217_Contains Duplicate.java
@@ -1,3 +1,4 @@
+//Basic loop version time complexity is O(n^2)
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
@@ -9,6 +10,26 @@ class Solution {
                     return true;
                 }
             }
+        }
+
+        return false;
+    }
+}
+// Hash Set version  O(n)
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+
+            seen.add(nums[i]);
         }
 
         return false;
